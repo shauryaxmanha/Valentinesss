a6481118cb01702e4f6742fd92c3de641d7e7a44
shauryaxmanha <shauryarana11@gmail.com>
2026-02-01 04:57:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -45,13 +45,13 @@ const PHOTO_URLS = [
 const LETTER_FROM = "Shaurya";
 const LETTER_BODY = `Happy Valentine’s Day, Manhaaaa 💗
 
-I don't know where to start because there is geniuenly so much to say. Good things BTW. Since the moment i met you
+I don't know where to start because there is genuinely so much to say. Good things BTW. Since the moment i met you
 , i knew you were special. Not just because of your crazy attractiveness, Its because of who  you are within.
-Something I picked up on very quickly was geniuenely how being around you made me feel so happy. 
+Something I picked up on very quickly was genuinely how being around you made me feel so happy. 
 Even though I had just barely knew you, the way you carried your self and your energy made me feel like I never wanted to leave.
 I knew very early on that I had liked you. WAY earlier than you realized, which is fine i guess.... I knew that this had to be more than just friends.I liked you for so many reasons,
-There is geniuenly no girl out there anymore that is like you and I really do mean that manha. I can go searching the whole world and not one would come even close
-to you. You are true to your self, you have respect for yourself, you are geniuenly the hardest worker I have ever seen, you are so smart, and you are literally good at everything you do.
+There is genuienly no girl out there anymore that is like you and I really do mean that manha. I can go searching the whole world and not one would come even close
+to you. You are true to your self, you have respect for yourself, you are genuienly the hardest worker I have ever seen, you are so smart, and you are literally good at everything you do.
 Except driving.... I am sorry if this is too soon to joke about. I will miss Cora everything will be fine tho. And i Am SOO Glad you and everyone is okay. I prayed and prayed and Thanked God.
 Oh and dont get me Started on how you look ONG from the moment i saw you i had never been more attracted to a girl in my life. Your skin is so clear, to this day I dont know if you are wearing
 makeup or not. OBV your eyes i can go on for days about them because they geniuenly are my favorite things in the world to look at. They actually seduce me bro like ONG i can stare at you for hours and hours.
@@ -60,14 +60,14 @@ able to find one. Like i actually dont know how i did it bro and I think about h
 Manha u care for me so much, and you are the best GF ever and ONG i mean that. You support me, You reassure me, You even put up with some of my BS arguments. Which obv U R always right. U calm me down when I overthink
 and you have shown to me that I can trust you and that you are always there for me. You have done things for me and showed love that I ddin't know anyone ever had for me, and you have done things i wouldnt even think of.
 Manha you are genuienly the sweetest girl ever and you care so much not even just me but for everyone. You stand up for others and you know whats right from wrong without a hesitation. Like the Sub incident
-where u comforted her, like you are actually an angel manha, u r so cute. Whenever you go through hard and stressful things, you always get over them, you are geniuenly the strongest girl i know manha, and 
+where u comforted her, like you are actually an angel manha, u r so cute. Whenever you go through hard and stressful things, you always get over them, you are genuinely the strongest girl i know manha, and 
 i hope you realize that there is nothing in this world i wouldnt do for you. ONG i would kill someone if you asked me to, you could kill me and i would still love you. I will always love you. Forever and ever,
 and love is not strong enough word to describe my feelings towards you. I actually miss you so much always even when i am with you because ik i have to leave soon. I need to live with you and we need to get married and have
 sleepovers forever. I know we will be that cute old couple. I cherrish all the moments we spend together so much and I always look back at when we were together and just smile. I acc cant stop i have auto smile. 
 You make me so happy manha more than you realize. I love your humor too u r ACC fuuny for a female... less than me but u get it. You acc make me laugh and i love talking to you. I used to hate FT but i love it now thats all 
 i want to do. I love your voice too, sos soothing, oh yes, i can do things to it rn. I am on call wit you RN you think im doing english project.... little do YK. I am sorry this is all over the place manha, this
 BS took me hours to do, i didn't know where to start and it made me restart everthing. It took me hours to do. Hopefully u like a little. I am so glad to be spending my first and last Valentines with you. I love you SOOOOO
-OOOOOOOOOOOO much baby, and i can genieunely talk abt my love for you and all the things i love abt u for hours but ik u dont want to read a harry potter book RN. I LOVE YOU BABY HAPPY VALENTINESSSS 
+OOOOOOOOOOOO much baby, and i can genuinely talk abt my love for you and all the things i love abt u for hours but ik u dont want to read a harry potter book RN. I LOVE YOU BABY HAPPY VALENTINESSSS 
 `;
 
 
